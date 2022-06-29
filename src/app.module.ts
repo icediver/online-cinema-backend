c42863e33a3e48d30e95dfe9efd40dfda7ec7ccc
IceDiver <admin@example.com>
2022-06-29 15:33:04 +0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GetMongoDbConfig } from './config/mongo.config';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypegooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: GetMongoDbConfig,
+    }),
+    AuthModule,
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+import { Auth } from 'src/auth/decorator/auth.decorator';
+import { User } from './decorators/user.decorator';
+import { UserService } from './user.service';
+
+@Controller('users')
+export class UserController {
+	constructor(private readonly userService: UserService) {}
+
+	@Get('profile')
+	@Auth()
+	async getProfile(@User('_id') _id: string) {
+		return this.userService.byId(_id)
+	}
+}
